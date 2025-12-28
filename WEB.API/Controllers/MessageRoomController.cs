09add553e782828b2c542f81e8f53363e2747857
Gökhan Çelebi <dinatopya03@gmail.com>
2025-12-28 16:16:27 +0300

    Add project files.

---
+++ WEB.API/Controllers/MessageRoomController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using Microsoft.AspNetCore.Mvc;
+using WEB.API.Hubs;
+
+namespace WEB.API.Controllers
+{
+    [ApiController]
+    [Route("api/message-room")]
+    public class MessageRoomController : ControllerBase
+    {
+        private readonly IHubContext<MessageHub> _hubContext;
+        public MessageRoomController(IHubContext<MessageHub> hubContext)
+        {
+            _hubContext = hubContext;
+        }
+        [HttpPost]
+        [Route("create")]
+        public async Task<IActionResult> CreateMessage()
+        {
+            var message = "Test";
+            await _hubContext.Clients.All
+                .SendAsync("MessageSend",message);
+
+            return Ok(new { Message = "Message Sent" });
+        }
+
+    }
+}
---
+++ WEB.API/Hubs/MessageHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace WEB.API.Hubs
+{
+    public class MessageHub : Hub
+    {
+        public async Task MessageSent(string message)
+        {
+            await Clients.All.SendAsync("MessageSend", message);
+        }
+    }
+}
